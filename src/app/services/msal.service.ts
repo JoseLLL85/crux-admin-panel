55ec8c6381b86d2769dae9f0cec385997ed0b0ad
Leon Lopez <Jose.LeonLopez@chubb.com>
2018-09-17 10:29:39 -0400

    AD B2C

--- src/app/services/msal.service.ts
+++ src/app/services/msal.service.ts
@@ -12,9 +12,9 @@ export class MsalService {
         tenant: "ChubbDigitalLADEV01.onmicrosoft.com",
         clientID: '7262c535-ad41-4ad0-a975-83734eba604f',
         signUpSignInPolicy: "B2C_1_PaneraTest",
-        b2cScopes: ["https://ChubbDigitalLADEV01.onmicrosoft.com/PaneraTemplate/user_impersonation"]
+        b2cScopes: ['https://ChubbDigitalLADEV01.onmicrosoft.com/PaneraTemplate/user_impersonation']
     };
-    
+
     // Configure the authority for Azure AD B2C
     authority = "https://login.microsoftonline.com/tfp/" + this.tenantConfig.tenant + "/" + this.tenantConfig.signUpSignInPolicy;
 
@@ -22,22 +22,31 @@ export class MsalService {
      * B2C SignIn SignUp Policy Configuration
      */
     clientApplication = new Msal.UserAgentApplication(
-        this.tenantConfig.clientID, this.authority, 
+        this.tenantConfig.clientID, this.authority,
         function (errorDesc: any, token: any, error: any, tokenType: any) {
             // Called after loginRedirect or acquireTokenPopup
         }
     );
 
     public login(): void {
+
         var _this = this;
         this.clientApplication.loginPopup(this.tenantConfig.b2cScopes).then(function (idToken: any) {
             _this.clientApplication.acquireTokenSilent(_this.tenantConfig.b2cScopes).then(
+
                 function (accessToken: any) {
+
                     _this.saveAccessTokenToCache(accessToken);
+
+
+                    console.log("token receibed successfuly :"+accessToken);
                 }, function (error: any) {
                     _this.clientApplication.acquireTokenPopup(_this.tenantConfig.b2cScopes).then(
                         function (accessToken: any) {
                             _this.saveAccessTokenToCache(accessToken);
+                            //sessionStorage.getItem(token_ident);
+
+
                         }, function (error: any) {
                             bootbox.alert("Error acquiring the popup:\n" + error);
                         });
@@ -56,6 +65,6 @@ export class MsalService {
     };
 
     isOnline(): boolean {
-        return this.clientApplication.getUser() != null; 
+        return this.clientApplication.getUser() != null;
     };
 }
